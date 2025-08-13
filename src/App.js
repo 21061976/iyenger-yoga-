30487cc51a04080bf76d3f69c18792d5effe73ba
21061976 <uzan76@gmail.com>
2025-08-13 09:29:46 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import IyengarYogaApp from './components/IyengarYogaApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <IyengarYogaApp />
+    </div>
+  );
+}
+
+export default App;
